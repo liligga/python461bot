c8f6dc71b7bdbf52196ce87c225c0686904ad844
igor <igglike@gmail.com>
2024-10-18 16:05:20 +0300

    :white_check_mark:Сделал диалог для админа для сохранения товаров

---
+++ handlers/admin_book_fsm.py
@@ -0,0 +1,71 @@
+from aiogram import Router, F, types
+from aiogram.filters import Command
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+
+from bot_config import database
+
+
+class BookForm(StatesGroup):
+    name = State()
+    price = State()
+    author = State()
+    genre = State()
+    confirm = State()
+
+admin = 243154734
+admin_book_router = Router()
+admin_book_router.message.filter(F.from_user.id == admin)
+
+
+@admin_book_router.message(Command("newbook"))
+async def start_book_form(message: types.Message, state: FSMContext):
+    await state.set_state(BookForm.name)
+    await message.answer("Задайте название книги:")
+
+
+@admin_book_router.message(BookForm.name)
+async def process_name(message: types.Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await state.set_state(BookForm.price)
+    await message.answer("Задайте цену книги:")
+
+
+@admin_book_router.message(BookForm.price)
+async def process_price(message: types.Message, state: FSMContext):
+    await state.update_data(price=message.text)
+    await state.set_state(BookForm.author)
+    await message.answer("Кто автор этой книги:")
+
+
+@admin_book_router.message(BookForm.author)
+async def process_author(message: types.Message, state: FSMContext):
+    await state.update_data(author=message.text)
+    await state.set_state(BookForm.genre)
+    await message.answer("Выберите жанр:")
+
+
+@admin_book_router.message(BookForm.genre)
+async def process_genre(message: types.Message, state: FSMContext):
+    await state.update_data(genre=message.text)
+    data = await state.get_data()
+    kb = types.ReplyKeyboardMarkup(
+        keyboard=[[
+            types.KeyboardButton(text="Да"),
+            types.KeyboardButton(text="Нет")
+        ]],
+        resize_keyboard=True
+    )
+    await state.set_state(BookForm.confirm)
+    await message.answer(f"Вы ввели:\n Название: {data['name']},\n Цена: {data['price']},\n"
+                         f"Автор: {data['author']},\n Жанр: {data['genre']}", reply_markup=kb)
+
+
+@admin_book_router.message(BookForm.confirm, F.text == "Да")
+async def process_confirm(message: types.Message, state: FSMContext):
+    data = await state.get_data()
+    print(data)
+    # save to db
+    await state.clear()
+    kb = types.ReplyKeyboardRemove()
+    await message.answer("Данные были сохранены!", reply_markup=kb)
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from handlers.start import start_router
 # from handlers.picture import picture_router
 from handlers.other_messages import other_msg_router
 from handlers.opros_dialog import opros_router
+from handlers.admin_book_fsm import admin_book_router
 
 
 async def on_startup(bot: Bot):
@@ -18,6 +19,7 @@ async def main():
     dp.include_router(start_router)
     # dp.include_router(picture_router)
     dp.include_router(opros_router)
+    dp.include_router(admin_book_router)
     # в самом конце общий обработчик
     dp.include_router(other_msg_router)
 
